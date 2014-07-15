e2ca7437bb3c43beabe031bdf344b901ddae4653
LPI2Grupo3 <tiagojcsousa92@gmail.com>
2014-07-15 01:00:19 +0100

    Create Configs.h
    Header file of Slave Code

---
+++ Configs.h
@@ -0,0 +1,21 @@
+/*
+ * Configs.h
+ *
+ * Created: 12/07/2014 22:24:25
+ *  Author: TiagoSousa
+ */ 
+
+#ifndef CONFIGS_H_
+#define CONFIGS_H_
+
+#define FOSC 16000000
+#define BAUD 38400
+#define MYUBR FOSC/16/BAUD-1
+
+void SerieSetup();
+void putbuf(char c);
+void TimerSetup();
+void ADCSetup();
+unsigned int Analog_Read(unsigned char chan);
+
+#endif /* CONFIGS_H_ */
